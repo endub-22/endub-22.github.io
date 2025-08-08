f559c29703e6d80d35bd66125c91e48715856947
endub-22 <120348961+endub-22@users.noreply.github.com>
2025-08-08 15:59:43 +1200

    Create sketch.js

---
+++ nebula-art/sketch.js
@@ -0,0 +1,38 @@
+let noiseScale = 0.005;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  pixelDensity(1);
+  noLoop();
+}
+
+function draw() {
+  loadPixels();
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      let n = noise(x * noiseScale, y * noiseScale);
+      // map noise to a colorful palette
+      let hue = map(n, 0, 1, 200, 320);  // tweak these
+      let sat = map(n, 0, 1, 50, 100);
+      let bri = map(n, 0, 1, 20, 100);
+      let col = color(`hsb(${hue}, ${sat}%, ${bri}%)`);
+      pixels[(x + y * width) * 4 + 0] = red(col);
+      pixels[(x + y * width) * 4 + 1] = green(col);
+      pixels[(x + y * width) * 4 + 2] = blue(col);
+      pixels[(x + y * width) * 4 + 3] = 255;
+    }
+  }
+  updatePixels();
+}
+
+// regenerate on click
+function mousePressed() {
+  noiseSeed(floor(random(10000)));
+  redraw();
+}
+
+// full-screen on resize
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  redraw();
+}
