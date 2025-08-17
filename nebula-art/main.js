9e8b4d8a53b74396d307495078956c6feda370bd
endub-22 <120348961+endub-22@users.noreply.github.com>
2025-08-18 10:17:44 +1200

    Update main.js

--- nebula-art/main.js
+++ nebula-art/main.js
@@ -10,7 +10,7 @@
   function startWhispers() {
     $storyUI.hidden = true;
     // sketch keeps running underneath
-    window.setWhispersEnabled(false);
+    window.setWhispersEnabled(true);
   }
 
   function startStory() {
